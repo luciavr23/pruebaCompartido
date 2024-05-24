7f1e4899d1b0405cb76b48b0bfa56dbc51249517
lucia <lucia.villar.rodriguez.alu@iesjulioverne.es>
2024-05-24 12:45:26 +0200

    version3

--- pruebaForGit/src/main/java/com/mycompany/pruebaforgit/PruebaForGit.java
+++ pruebaForGit/src/main/java/com/mycompany/pruebaforgit/PruebaForGit.java
@@ -12,6 +12,6 @@ public class PruebaForGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Probando Fork");
+        System.out.println("Probando .....Fork");
     }
 }
